e5d599d51449e0736c2fdef25c8e93741c345db7
kerbsod <kerbsod@gmail.com>
2024-08-06 00:36:32 -0300

    - Added more dates for the complete case given in the example - changed date names to be more declarative - ElementsMatch asserts if two slices have the same elements. The problem with equal was that the order of elements in a slice do matter and the 'map' in go changes the order of its iterations on each run so we don't assume the order. This unpredictable order asserts some tests and fails others on each run. So we now test if developers contain the same dates regardless of the order.

--- pkg/project_test.go
+++ pkg/project_test.go
@@ -9,123 +9,156 @@ import (
 // Developers
 var danIngalls = NewDeveloper("Dan Ingalls", 8, 60)
 var alanKay = NewDeveloper("Alan Kay", 6, 80)
+var adeleGoldberg = NewDeveloper("Adele Goldberg", 10, 65)
 
 // Teams
 var danTeam = NewTeam("Dan team", []Responsible{danIngalls})
 var parcMobTeam = NewTeam("Parc mob team", []Responsible{danIngalls, alanKay})
 
 // Dates
-var julyFirst = time.Date(2024, time.July, 1, 0, 0, 0, 0, time.UTC)
-var julyTheSecond = time.Date(2024, time.July, 2, 0, 0, 0, 0, time.UTC)
-var julyTheThird = time.Date(2024, time.July, 3, 0, 0, 0, 0, time.UTC)
+var july1th = time.Date(2024, time.July, 1, 0, 0, 0, 0, time.UTC)
+var july2th = time.Date(2024, time.July, 2, 0, 0, 0, 0, time.UTC)
+var july3th = time.Date(2024, time.July, 3, 0, 0, 0, 0, time.UTC)
+var july4th = time.Date(2024, time.July, 4, 0, 0, 0, 0, time.UTC)
+var july5th = time.Date(2024, time.July, 5, 0, 0, 0, 0, time.UTC)
+var july6th = time.Date(2024, time.July, 6, 0, 0, 0, 0, time.UTC)
+var july7th = time.Date(2024, time.July, 7, 0, 0, 0, 0, time.UTC)
 
 func Test01ConcreteTaskFinishesInADayIfDeveloperDedicationsIsTaskEffort(t *testing.T) {
-	task := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	assert.Equal(t, julyFirst, task.FinishDate())
+	task := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	assert.Equal(t, july1th, task.FinishDate())
 }
 
 func Test02ConcreteTaskDoesNotFinishesInADayIfDeveloperDedicationIsLessThanTaskEffort(t *testing.T) {
-	task := NewConcreteTask("SS A", alanKay, julyFirst, 8, []Task{})
-	assert.Equal(t, julyTheSecond, task.FinishDate())
+	task := NewConcreteTask("SS A", alanKay, july1th, 8, []Task{})
+	assert.Equal(t, july2th, task.FinishDate())
 }
 
 func Test03ConcreteTaskStartsOnAfterDependentsFinish(t *testing.T) {
-	task1 := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	task2 := NewConcreteTask("SS B", danIngalls, julyFirst, 8, []Task{task1})
-	assert.Equal(t, julyTheSecond, task2.FinishDate())
+	task1 := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	task2 := NewConcreteTask("SS B", danIngalls, july1th, 8, []Task{task1})
+	assert.Equal(t, july2th, task2.FinishDate())
 }
 
 func Test04ConcreteTaskDoesNotStartBeforeDesiredStartingDate(t *testing.T) {
-	task1 := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	task2 := NewConcreteTask("SS B", alanKay, julyTheThird, 8, []Task{task1})
-	assert.Equal(t, julyTheThird, task2.StartDate())
+	task1 := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	task2 := NewConcreteTask("SS B", alanKay, july3th, 8, []Task{task1})
+	assert.Equal(t, july3th, task2.StartDate())
 }
 
 func Test05ConcreteTaskDoesNotStartTheSameDayItsDependentsEnd(t *testing.T) {
-	task1 := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	task2 := NewConcreteTask("SS B", danIngalls, julyFirst, 8, []Task{task1})
-	assert.NotEqual(t, julyFirst, task2.StartDate())
+	task1 := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	task2 := NewConcreteTask("SS B", danIngalls, july1th, 8, []Task{task1})
+	assert.NotEqual(t, july1th, task2.StartDate())
 }
 
 func Test06ConcreteTaskStartsAfterGreatestFinishDateInDependents(t *testing.T) {
-	task1 := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	task2 := NewConcreteTask("SS B", danIngalls, julyFirst, 16, []Task{})
-	task3 := NewConcreteTask("SS C", danIngalls, julyFirst, 16, []Task{task1, task2})
-	assert.Equal(t, julyTheThird, task3.StartDate())
+	task1 := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	task2 := NewConcreteTask("SS B", danIngalls, july1th, 16, []Task{})
+	task3 := NewConcreteTask("SS C", danIngalls, july1th, 16, []Task{task1, task2})
+	assert.Equal(t, july3th, task3.StartDate())
 }
 
 func Test07ConcreteTaskTimeToFinishForTeamWithOneDeveloperIsDeveloperTimeToFinish(t *testing.T) {
-	task := NewConcreteTask("SS A", danTeam, julyFirst, 16, []Task{})
-	assert.Equal(t, julyTheSecond, task.FinishDate())
+	task := NewConcreteTask("SS A", danTeam, july1th, 16, []Task{})
+	assert.Equal(t, july2th, task.FinishDate())
 }
 
 func Test08ConcreteTaskTimeToFinishDependsOnSlowestDeveloper(t *testing.T) {
-	task := NewConcreteTask("SS A", parcMobTeam, julyFirst, 16, []Task{})
-	assert.Equal(t, julyTheThird, task.FinishDate())
+	task := NewConcreteTask("SS A", parcMobTeam, july1th, 16, []Task{})
+	assert.Equal(t, july3th, task.FinishDate())
 }
 
 func Test09ProjectStartDateWithOneSubTaskIsSubtaskStartDate(t *testing.T) {
-	task := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
+	task := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
 	project := NewProject("UI", []Task{task})
-	assert.Equal(t, julyFirst, project.StartDate())
+	assert.Equal(t, july1th, project.StartDate())
 }
 
 func Test10ProjectStartDateIsSubtasksEarliestStartDate(t *testing.T) {
-	taskSSA := NewConcreteTask("SS A", danIngalls, julyTheThird, 8, []Task{})
-	taskSSB := NewConcreteTask("SS B", parcMobTeam, julyTheSecond, 16, []Task{})
+	taskSSA := NewConcreteTask("SS A", danIngalls, july3th, 8, []Task{})
+	taskSSB := NewConcreteTask("SS B", parcMobTeam, july2th, 16, []Task{})
 	project := NewProject("Modelo", []Task{taskSSA, taskSSB})
-	assert.Equal(t, julyTheSecond, project.StartDate())
+	assert.Equal(t, july2th, project.StartDate())
 }
 
 func Test11ProjectFinishDateWithOneSubTaskIsSubtaskFinishDate(t *testing.T) {
-	task := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
+	task := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
 	project := NewProject("UI", []Task{task})
-	assert.Equal(t, julyFirst, project.FinishDate())
+	assert.Equal(t, july1th, project.FinishDate())
 }
 
 func Test12ProjectFinishDateIsSubtasksLatestFinishDate(t *testing.T) {
-	taskSSA := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	taskSSB := NewConcreteTask("SS B", parcMobTeam, julyFirst, 16, []Task{})
+	taskSSA := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	taskSSB := NewConcreteTask("SS B", parcMobTeam, july1th, 16, []Task{})
 	project := NewProject("Modelo", []Task{taskSSA, taskSSB})
-	assert.Equal(t, julyTheThird, project.FinishDate())
+	assert.Equal(t, july3th, project.FinishDate())
 }
 
 func Test13DeveloperWithoutOverAssignmentsReturnsEmptyCollection(t *testing.T) {
-	taskSSA := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
+	taskSSA := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
 	project := NewProject("Modelo", []Task{taskSSA})
 	worksheet := project.Worksheet()
 
-	overassignments := make(map[*Developer][]time.Time)
-	overassignments[danIngalls] = []time.Time{}
+	overassignments := make(map[*Developer][]map[time.Time]int)
+	overassignments[danIngalls] = []map[time.Time]int{
+		map[time.Time]int{july1th: 1},
+	}
 
-	assert.Equal(t, overassignments, worksheet.Overassignments())
+	assert.ElementsMatch(t, worksheet.Overassignments()[danIngalls], overassignments[danIngalls])
 }
 
 func Test14DeveloperWithOverassignmentsReturnsArrayWithOverassignedDays(t *testing.T) {
-	taskSSA := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	taskSSB := NewConcreteTask("SS B", parcMobTeam, julyFirst, 16, []Task{})
+	taskSSA := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	taskSSB := NewConcreteTask("SS B", parcMobTeam, july1th, 16, []Task{})
 	project := NewProject("Modelo", []Task{taskSSA, taskSSB})
 	worksheet := project.Worksheet()
 
-	overassignments := make(map[*Developer][]time.Time)
-	overassignments[danIngalls] = []time.Time{julyFirst}
-	overassignments[alanKay] = []time.Time{}
+	overassignments := make(map[*Developer][]map[time.Time]int)
+	overassignments[danIngalls] = []map[time.Time]int{
+		map[time.Time]int{july1th: 2},
+		map[time.Time]int{july2th: 1},
+		map[time.Time]int{july3th: 1},
+	}
+	overassignments[alanKay] = []map[time.Time]int{
+		map[time.Time]int{july1th: 1},
+		map[time.Time]int{july2th: 1},
+		map[time.Time]int{july3th: 1},
+	}
 
-	assert.Equal(t, overassignments, worksheet.Overassignments())
+	assert.ElementsMatch(t, worksheet.Overassignments()[danIngalls], overassignments[danIngalls])
+	assert.ElementsMatch(t, worksheet.Overassignments()[alanKay], overassignments[alanKay])
 }
 
-/*
 func Test15DevelopersWithOverassignmentsReturnsArrayWithOverassignedDaysPerDeveloper(t *testing.T) {
-	taskSSA := NewConcreteTask("SS A", danIngalls, julyFirst, 8, []Task{})
-	taskSSB := NewConcreteTask("SS B", alanKay, julyFirst, 6, []Task{})
-	taskSSC := NewConcreteTask("SS C", parcMobTeam, julyFirst, 6, []Task{})
-	project := NewProject("Modelo", []Task{taskSSA, taskSSB, taskSSC})
-	worksheet := project.Worksheet()
+	SSA := NewConcreteTask("SS A", danIngalls, july1th, 8, []Task{})
+	SSB := NewConcreteTask("SS B", parcMobTeam, july1th, 16, []Task{})
+	SSC := NewConcreteTask("SS C", alanKay, july2th, 16, []Task{SSA, SSB})
+	model := NewProject("Modelo", []Task{SSA, SSB, SSC})
+	UI := NewConcreteTask("UI", adeleGoldberg, july2th, 6, []Task{model})
+	systemERP := NewProject("Sistema ERP", []Task{model, UI})
 
-	overassignments := make(map[*Developer][]time.Time)
-	overassignments[danIngalls] = []time.Time{julyFirst}
-	overassignments[alanKay] = []time.Time{julyFirst}
+	worksheet := systemERP.Worksheet()
 
-	assert.Equal(t, worksheet.Overassignments(), overassignments)
+	overassignments := make(map[*Developer][]map[time.Time]int)
+	overassignments[danIngalls] = []map[time.Time]int{
+		map[time.Time]int{july1th: 2},
+		map[time.Time]int{july2th: 1},
+		map[time.Time]int{july3th: 1},
+	}
+	overassignments[alanKay] = []map[time.Time]int{
+		map[time.Time]int{july1th: 1},
+		map[time.Time]int{july2th: 1},
+		map[time.Time]int{july3th: 1},
+		map[time.Time]int{july4th: 1},
+		map[time.Time]int{july5th: 1},
+		map[time.Time]int{july6th: 1},
+	}
+	overassignments[adeleGoldberg] = []map[time.Time]int{
+		map[time.Time]int{july7th: 1},
+	}
+
+	assert.ElementsMatch(t, worksheet.Overassignments()[danIngalls], overassignments[danIngalls])
+	assert.ElementsMatch(t, worksheet.Overassignments()[alanKay], overassignments[alanKay])
+	assert.ElementsMatch(t, worksheet.Overassignments()[adeleGoldberg], overassignments[adeleGoldberg])
 }
-*/
